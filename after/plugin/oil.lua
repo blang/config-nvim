ae1bcfb060cf44e3dcbf50aa7bf5c5e57f1ef73f
Lang, Benedikt, Vodafone DE <benedikt.lang1@vodafone.com>
2023-08-17 03:12:19 +0200

    feat: configure oil to be floating

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,2 +1,24 @@
-require("oil").setup()
-vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
+require("oil").setup({
+    columns = {
+        "icon",
+        "permissions",
+        "size",
+        -- "mtime",
+    },
+    float = {
+        -- Padding around the floating window
+        padding = 2,
+        max_width = 100,
+        max_height = 50,
+        border = "rounded",
+        win_options = {
+            winblend = 10,
+        },
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        override = function(conf)
+            return conf
+        end,
+    },
+})
+vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory" })
